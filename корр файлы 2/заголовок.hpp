342873c5488526596c7247f0476e5ea147269883
Otec <OtecDota2>
2025-01-24 18:14:08 +0500

    Добавьте файлы проекта.

---
+++ корр файлы 2/заголовок.hpp
@@ -0,0 +1,15 @@
+#pragma once;
+#ifndef some_funcion_hpp
+#define some_funcion_hpp
+#endif // !some_funcion_hpp
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+#include <cstdlib>
+#include <cstring>
+using namespace std;
+
+void Print(char* arr, int length);//объявление функции печать строки
---
+++ корр файлы 2/функции.cpp
@@ -0,0 +1,12 @@
+#include "заголовок.hpp"
+
+void Print(char* arr, int length)
+{
+
+	for (int i = 0; i < length; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << '\n';
+
+}
